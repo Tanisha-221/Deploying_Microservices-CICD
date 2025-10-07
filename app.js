52a463dcc51845cb0738aaf349886ade4b5f7594
Tanisha-221 <tanishajindal223@gmail.com>
2025-10-07 12:03:09 +0530

    aading terraform file, docker image and node js file

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello Azure DevOps CI/CD!'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
